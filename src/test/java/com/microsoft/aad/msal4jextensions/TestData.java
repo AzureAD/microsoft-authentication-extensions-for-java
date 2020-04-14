b3cc3484db835b1d7745ab20e25f749d50e2978c
SomkaPe <pesomka@microsoft.com>
2020-04-14 00:08:00 -0700

    minor fix

--- src/test/java/com/microsoft/aad/msal4jextensions/TestData.java
+++ src/test/java/com/microsoft/aad/msal4jextensions/TestData.java
@@ -7,6 +7,6 @@ public class TestData {
 
     static String GRAPH_DEFAULT_SCOPE = "https://graph.windows.net/.default";
 
-    static String CONFIDENTIAL_CLIENT_ID = "118895f6-1c0c-4f95-ae54-490c534d7f97";
-    static String CONFIDENTIAL_CLIENT_SECRET = "vC-Zzk@Kl|=HO-;15QR)d.[chry_!e@|w#CcL{5%y+{dx99m!E}w";
+    static String CONFIDENTIAL_CLIENT_ID = "";
+    static String CONFIDENTIAL_CLIENT_SECRET = "";
 }
